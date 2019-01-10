7d3106661b4d50d9603c8fd78e81f1e1f61852dc
Jakov Perica <jakov.perica@qoitech.com>
2019-01-10 14:43:10 +0100

    Add routing with react router.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Browse from './components/Browse'
+import Random from './components/Random'
+import Search from './components/Search'
 class App extends Component {
   render() {
     return (
-      <div>
-        Clean hello world
-      </div>
+      <Router>
+        <div>
+          <div>
+            Menu
+          </div>
+          <Route exact path="/" component={Browse} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/random" component={Random} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Random/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Random() {
+    return (
+        <div>
+            Random
+        </div>
+    )
+}
+
+export default Random;
