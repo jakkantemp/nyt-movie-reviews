a256e02802035e98c334fa00cb6b162524c3905a
Jakov Perica <jakov.perica@qoitech.com>
2019-01-11 08:30:44 +0100

    Fetch from the api.

--- src/components/Browse/index.js
+++ src/components/Browse/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Browse() {
-    return (
-        <div>
-            Browse
-        </div>
-    )
+class Browse extends Component {
+    componentDidMount() {
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?&api-key=${process.env.REACT_APP_API_KEY}`, {
+            method: "GET",
+            headers:{
+                'Content-Type': 'application/json',
+              }
+        }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+    }
+
+    render() {
+        return (
+            <div>
+                Browse
+            </div>
+        )
+    }
 }
 
 export default Browse;
