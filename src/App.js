60d266ca24c1ffe44d121e83122c7c00259b09fb
Jakov Perica <jakov.perica@qoitech.com>
2019-01-11 12:21:01 +0100

    Remove unsupported route.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Browse from './components/Browse'
 import Menu from './components/Menu'
-import Random from './components/Random'
 import Search from './components/Search'
 class App extends Component {
   render() {
@@ -15,7 +14,6 @@ class App extends Component {
           </div>
           <Route exact path="/" component={Browse} />
           <Route exact path="/search" component={Search} />
-          <Route exact path="/random" component={Random} />
         </div>
       </Router>
     );
